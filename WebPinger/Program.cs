d4f4bcecf0d51f0783dcfc9e79a63f0265398a00
Navendu <navenduk75@yahoo.com>
2016-03-27 17:00:01 -0400

    Initial

---
+++ WebPinger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using log4net;
+
+namespace WebPinger
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            log4net.Config.BasicConfigurator.Configure();
+
+            var ping = new Ping();
+            var result = ping.Send("webapi.swamaanshaadi.com");
+
+            Log.Info("Status: " + result.Status.ToString());
+
+            //if (result.Status != IPStatus.Success)
+            //    return;
+        }
+    }
+}
